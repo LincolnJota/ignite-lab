46ee99143a7c7b72993880e569ce377decf4deb5
LincolnJota <contato.joydamasceno@gmail.com>
2022-06-23 23:12:20 -0300

    ...

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -66,7 +66,7 @@ export function Video(props: VideoParams) {
       <div className="bg-black flex justify-center">
         <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
           <Player>
-            <Youtube videoId={data!.lesson.videoId} key={data!.lesson.videoId} />
+            <Youtube videoId={data!.lesson.videoId} />
             <DefaultUi />
 
           </Player>
